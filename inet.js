e1ef6825f74b64d7f8e17775aab5265fc66e20a3
clowwindy <clowwindy42@gmail.com>
2013-05-16 14:21:06 +0800

    support IPv6 address

---
+++ inet.js
@@ -0,0 +1,99 @@
+// The functions in source file is from phpjs
+// https://github.com/kvz/phpjs
+// See license below
+//
+// Copyright (c) 2013 Kevin van Zonneveld (http://kvz.io) 
+// and Contributors (http://phpjs.org/authors)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//   SOFTWARE.
+
+
+function inet_pton (a) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Theriault
+  // *     example 1: inet_pton('::');
+  // *     returns 1: '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0' (binary)
+  // *     example 2: inet_pton('127.0.0.1');
+  // *     returns 2: '\x7F\x00\x00\x01' (binary)
+  var r, m, x, i, j, f = String.fromCharCode;
+  m = a.match(/^(?:\d{1,3}(?:\.|$)){4}/); // IPv4
+  if (m) {
+    m = m[0].split('.');
+    m = f(m[0]) + f(m[1]) + f(m[2]) + f(m[3]);
+    // Return if 4 bytes, otherwise false.
+    return m.length === 4 ? m : false;
+  }
+  r = /^((?:[\da-f]{1,4}(?::|)){0,8})(::)?((?:[\da-f]{1,4}(?::|)){0,8})$/;
+  m = a.match(r); // IPv6
+  if (m) {
+    // Translate each hexadecimal value.
+    for (j = 1; j < 4; j++) {
+      // Indice 2 is :: and if no length, continue.
+      if (j === 2 || m[j].length === 0) {
+        continue;
+      }
+      m[j] = m[j].split(':');
+      for (i = 0; i < m[j].length; i++) {
+        m[j][i] = parseInt(m[j][i], 16);
+        // Would be NaN if it was blank, return false.
+        if (isNaN(m[j][i])) {
+          return false; // Invalid IP.
+        }
+        m[j][i] = f(m[j][i] >> 8) + f(m[j][i] & 0xFF);
+      }
+      m[j] = m[j].join('');
+    }
+    x = m[1].length + m[3].length;
+    if (x === 16) {
+      return m[1] + m[3];
+    } else if (x < 16 && m[2].length > 0) {
+      return m[1] + (new Array(16 - x + 1)).join('\x00') + m[3];
+    }
+  }
+  return false; // Invalid IP.
+}
+
+function inet_ntop (a) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Theriault
+  // *     example 1: inet_ntop('\x7F\x00\x00\x01');
+  // *     returns 1: '127.0.0.1'
+  // *     example 2: inet_ntop('\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\1');
+  // *     returns 2: '::1'
+  var i = 0,
+    m = '',
+    c = [];
+  a += '';
+  if (a.length === 4) { // IPv4
+    return [
+    a.charCodeAt(0), a.charCodeAt(1), a.charCodeAt(2), a.charCodeAt(3)].join('.');
+  } else if (a.length === 16) { // IPv6
+    for (i = 0; i < 16; i++) {
+      c.push(((a.charCodeAt(i++) << 8) + a.charCodeAt(i)).toString(16));
+    }
+    return c.join(':').replace(/((^|:)0(?=:|$))+:?/g, function (t) {
+      m = (t.length > m.length) ? t : m;
+      return t;
+    }).replace(m || ' ', '::');
+  } else { // Invalid length
+    return false;
+  }
+}
+exports.inet_pton = inet_pton;
+exports.inet_ntop = inet_ntop;
